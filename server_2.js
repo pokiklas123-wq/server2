933cd0d45a280eda902b95062734fe6ff7edd07a
pokiklas123-wq <pokiklas123@gmail.com>
2026-01-05 23:08:06 +0100

    Add files via upload

--- server_2.js
+++ server_2.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 // ==================== متغيرات البيئة ====================
 const PORT = process.env.PORT || 10000;
-const DATABASE_SECRETS = process.env.DATABASE_SECRETS;
-const DATABASE_URL = process.env.DATABASE;
+const DATABASE_SECRETS = "KXPNxnGZDA1BGnzs4kZIA45o6Vr9P5nJ3Z01X4bt";
+const DATABASE_URL = "https://hackerdz-b1bdf.firebaseio.com";
 const SERVER_3_URL = process.env.SERVER_3_URL; // متغير بيئة جديد للاتصال بالبوت 3
 
 const FIXED_DB_URL = DATABASE_URL && !DATABASE_URL.endsWith('/') ? DATABASE_URL + '/' : DATABASE_URL;
